6e5d4b6fe3983e609a0346a969766302385f3488
Emptea <aryas1725@gmail.com>
2026-07-23 20:45:36 +0300

    set ext clk for both & higher amp for dg1022a

--- setup_gens.py
+++ setup_gens.py
@@ -3,9 +3,10 @@ from afg3102c import *
 
 gen_46u4 = DG1022A()
 gen_46u4.set_ext_clk()
-gen_46u4.set_pulse(1e6/46.4, 1, 0, 0)
+gen_46u4.set_pulse(1e6/46.4, 3, 0, 0)
 gen_46u4.output(1)
 
 gen_40mhz = AFG3102C()
+gen_40mhz.set_ext_clk()
 gen_40mhz.set_sin(40, 3, 0, 0)
 gen_40mhz.output(1)
