f8203c72b1e062713c6cbea4b92737e10e5d5767
Emptea <aryas1725@gmail.com>
2026-07-24 20:20:59 +0300

    add readme

--- setup_gens.py
+++ setup_gens.py
@@ -1,12 +1,11 @@
 from dg1022a import *
 from afg3102c import *
+gen_40mhz = AFG3102C()
+gen_40mhz.set_ext_clk()
+gen_40mhz.set_sin(40, 3, 0, 0)
+gen_40mhz.output(1)
 
 gen_46u4 = DG1022A()
 gen_46u4.set_ext_clk()
 gen_46u4.set_pulse(1e6/46.4, 3, 0, 0)
 gen_46u4.output(1)
-
-gen_40mhz = AFG3102C()
-gen_40mhz.set_ext_clk()
-gen_40mhz.set_sin(40, 3, 0, 0)
-gen_40mhz.output(1)
